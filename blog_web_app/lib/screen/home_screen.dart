4d8aad2ae855970d0896cc41e3badaac3ae1b7bb
DK <dongwook5478@gmail.com>
2023-07-19 20:37:37 +0900

    update home button

--- blog_web_app/lib/screen/home_screen.dart
+++ blog_web_app/lib/screen/home_screen.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  WebViewController? controller;
+
+  HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('HomeScreen'),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF02343F),
+        title: Text("Daniel Kim's appBar"),
+        centerTitle: true,
+
+        actions: [
+          IconButton(onPressed: (){
+            controller!=null? controller!.loadUrl("https://github.com/bladnoch/codefactory-flutter"): {};
+          }, icon: Icon(
+            Icons.home,
+          ),
+          ),
+        ],
+      ),
+      body: WebView(
+        onWebViewCreated: (WebViewController controller){
+          this.controller=controller;
+      },
+        initialUrl:"https://github.com/bladnoch/codefactory-flutter",
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
     );
   }
 }
