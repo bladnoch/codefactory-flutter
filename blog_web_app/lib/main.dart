f8f070baece371c7b8b5ee2685dbbc837a48e160
DK <dongwook5478@gmail.com>
2023-07-19 20:47:10 +0900

    update comment

--- blog_web_app/lib/main.dart
+++ blog_web_app/lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:blog_web_app/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 
+/// main.dart
+/// @author DoungukKim
+/// @since 7/19/2023
+/// @version V0.0.1
+
 void main() {
   runApp(
       MaterialApp(
--- blog_web_app/lib/screen/home_screen.dart
+++ blog_web_app/lib/screen/home_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+/// home_screen.dart : HomeScreen
+/// @author DoungukKim
+/// @since 7/19/2023
+/// @version V0.0.1
 
 class HomeScreen extends StatelessWidget {
+  //controller 정보를 받기위한 변수
   WebViewController? controller;
 
   HomeScreen({super.key});
@@ -15,8 +20,13 @@ class HomeScreen extends StatelessWidget {
         title: Text("Daniel Kim's appBar"),
         centerTitle: true,
 
+        //앱바 안에 홈버튼
         actions: [
           IconButton(onPressed: (){
+            /**
+             * controller이 null이 아닐경우 실행
+             * 실행이 될경우의 조건이 controller가 절대 null일 수 없기 때문에 controller!로 null일 수 없음을 표현
+             */
             controller!=null? controller!.loadUrl("https://github.com/bladnoch/codefactory-flutter"): {};
           }, icon: Icon(
             Icons.home,
@@ -24,7 +34,9 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
+      //웹뷰: 웹사이트 어플로 디스플레이
       body: WebView(
+        //웝뷰가 생성 되었을 때 실행
         onWebViewCreated: (WebViewController controller){
           this.controller=controller;
       },
