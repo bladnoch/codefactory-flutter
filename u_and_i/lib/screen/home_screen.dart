f048e0f324bb9c402ede0a90bffd870a9272e0f1
DK <dongwook5478@gmail.com>
2023-07-24 01:35:54 +0900

    add background image

--- u_and_i/lib/screen/home_screen.dart
+++ u_and_i/lib/screen/home_screen.dart
@@ -6,7 +6,41 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("home screen"),
+      backgroundColor: Colors.pink[100],
+      body: SafeArea(
+        top: true,
+        bottom: false,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            _DDay(),
+            _CoupleImage(),
+          ],
+        ),
+      ),
     );
   }
 }
+
+class _DDay extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Text("DDAy widget");
+  }
+}
+
+class _CoupleImage extends StatelessWidget {
+  const _CoupleImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset('asset/img/middle_image.png',
+        ///이미지의 사이즈를 전체 화면 사이즈의 절만으로 설정
+      height: MediaQuery.of(context).size.height/2,
+      ),
+    );
+  }
+}
+
